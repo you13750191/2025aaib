9d426c36330c183f95d4766c23436cd7851b0ec7
you13750191 <13750191@me.mcu.edu.tw>
2025-02-19 11:55:25 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,14 @@
+///week01-1.cpp瘋狂程設 兩數相加
+///file-new empty開新檔案
+#include <stdio.h>///檔名不能寫錯
+///stdio的意思是標準standard input/output
+///有了stdio.h外掛才能用scanf()printf()
+int main()
+{
+	int a,b;///整數a,b
+	scanf("%d",&a);///剛剛教的
+	scanf("%d",&b);///可以複製的再改b
+
+	printf("%d",a+b);///印東西
+
+}
